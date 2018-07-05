ee2309e703bccec9ad61f74bfbd1915d2cd779e8
dinhvankiet124 <dinhvankiet124@gmail.com>
2018-07-05 23:47:08 +0700

    First commit

---
+++ mathpix/test.py
@@ -0,0 +1,21 @@
+import base64
+import json
+import time
+
+import requests
+
+start = time.time()
+file_path = 'images/anh.jpg'
+with open(file_path, "rb") as f:
+    encode = str(base64.b64encode(f.read()))
+image_uri = 'data:image/jpg;base64,' + encode[2:-1]
+data_json = '{"src": "' + image_uri + '", "formats": ["latex_styled"]}'
+r = requests.post("https://api.mathpix.com/v3/latex",
+                  data=data_json,
+                  headers={"app_id": "dinhvankiet124_gmail_com", "app_key": "7c7786b646d3bd31a0f2",
+                           "Content-type": "application/json"})
+
+print(time.time() - start)
+res = json.loads(r.text)
+print(res)
+print(res['latex_styled'])
